ca25902b045f0e870d7935b53397e6d6597079e3
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-01 01:07:45 +0100

    Created TodoList Component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,24 @@
+import React from "react";
+import Todo from "./Todo";
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+class TodoList extends React.Component {
+  render() {
+    const { todos, onToggleComplete } = this.props;
+    return (
+      <ul className="todo-list">
+        {todos.map(todo => (
+          <Todo
+            isCompleted={todo.isCompleted}
+            id={todo.id}
+            onToggleComplete={onToggleComplete}
+            title={todo.title}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
